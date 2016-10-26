4126a9d4bc92e5b404a4c60e5e7c1a8fa8cf7fd0
verne <chengdezuoweichen@qq.com>
2016-10-26 16:29:54 +0800

    添加forms.py

---
+++ diaries/forms.py
@@ -0,0 +1,5 @@
+#!-*encoding:utf-8*-
+from django import forms
+class diaryform(forms.Form):
+    diary_title=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'请键入您的标题' }),max_length=30)
+    diary_text=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'请键入您的日记' }),max_length=2000)
