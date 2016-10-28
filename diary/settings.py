99e78b33664335a89c871825341b9edc9cabd7f0
verne <chengdezuoweichen@qq.com>
2016-10-28 14:48:30 +0800

    规范修改

--- diary/settings.py
+++ diary/settings.py
@@ -80,10 +80,8 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diary',
-        'USER':'root',
-        'PASSWORD':'zxczxc',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
